414f6c29ca5ba5e4d0fde8ab7515355b937f85d0
shonenada <shonenada@gmail.com>
2021-03-15 14:45:13 +0800

    feat(arke-ws): add upstream into state

--- apps/arke_ws/src/arke_ws_proxy.erl
+++ apps/arke_ws/src/arke_ws_proxy.erl
@@ -5,26 +5,38 @@
 
 -record(state, {
           service :: binary(),
-          session_pid :: pid()
+          session_pid :: pid(),
+          upstream :: string()
          }).
 
 init(Req, _Opts) ->
+    Service = cowboy_req:binding(service, Req),
+    {ok, Upstream} = find_upstream(Service),
     {cowboy_websocket, Req,
      #state{
-        service = cowboy_req:binding(service, Req)
+        service = Service,
+        upstream = Upstream
        }}.
 
 websocket_init(_State) ->
     process_flag(trap_exit, true),
     {ok, _State}.
 
-websocket_handle({text, Data} = Frame, _State) ->
+websocket_handle({text, Data} = _Frame, _State) ->
     io:format("~p", [Data]),
     {ok, _State}.
 
-websocket_info({'DOWN', _, process, Pid, _}, _State) ->
+websocket_info({'DOWN', _, process, _Pid, _}, _State) ->
     {stop, _State}.
 
 terminate(Reason, _Req, _State) ->
     io:format("~s", Reason),
     ok.
+
+find_upstream(Stream) ->
+    case Stream of
+        <<"nginx">> ->
+            {ok, "http://127.0.0.1:80"};
+         _ ->
+            {error, not_found}
+    end.
